64c2214b65e2c992bb23f192b23adbb8cdf0581d
Fei0716 <ahfeiahfeiahfei@gmail.com>
2024-07-13 12:01:46 +0800

    Test Library

---
+++ resources/views/test.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>UWDC2024</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+        <button class="btn btn-primary">TEst</button>
+</body>
+</html>
